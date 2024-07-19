d6ae2c7d1c84811f41cfd94ac527f0a02b48d054
JazzPants14 <leighton.l14@gmail.com>
2024-07-19 17:03:41 +1000

    add superhero model and service

---
+++ SuperHeroAPI/Models/Superhero.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+
+namespace SuperHeroAPI.Models
+{
+    public class Superhero
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string SuperPower { get; set; } = null!;
+
+        public string Location { get; set; } = null!;
+
+        [BsonElement("items")]
+        [JsonPropertyName("items")]
+        public List<string> Images { get; set; } = null!;
+    }
+}
---
+++ SuperHeroAPI/Services/MongoDBService.cs
@@ -0,0 +1,22 @@
+﻿using SuperHeroAPI.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+
+namespace SuperHeroAPI.Services
+{
+    public class MongoDBService
+    {
+
+        private readonly IMongoCollection<Superhero> _superheroCollection;
+
+        public MongoDBService(IOptions<MongoDBSettings> mongoDBSettings)
+        {
+            MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURI);
+            IMongoDatabase database = client.GetDatabase(mongoDBSettings.Value.DatabaseName);
+            _superheroCollection = database.GetCollection<Superhero>(mongoDBSettings.Value.CollectionName);
+        }
+
+    }
+}
