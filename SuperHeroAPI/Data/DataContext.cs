e813b5c214ffa163bfaff3e7df86e9ec5519bcf1
JazzPants14 <leighton.l14@gmail.com>
2024-07-19 20:44:42 +1000

    retire datacontext from EF

--- SuperHeroAPI/Data/DataContext.cs
+++ SuperHeroAPI/Data/DataContext.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿//using Microsoft.EntityFrameworkCore;
 
-namespace SuperHeroAPI.Data
-{
-    public class DataContext : DbContext
-    {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+//namespace SuperHeroAPI.Data
+//{
+//    public class DataContext : DbContext
+//    {
+//        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
-    }
-}
+//        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
+//    }
+//}
