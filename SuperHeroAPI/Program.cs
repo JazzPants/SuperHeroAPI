0a1f6a22369b08e9e14d4acb908002085adfebdb
JazzPants14 <leighton.l14@gmail.com>
2024-07-20 15:21:22 +1000

    add back cors policy for local, comment out migrations

--- SuperHeroAPI/Program.cs
+++ SuperHeroAPI/Program.cs
@@ -11,18 +11,20 @@ builder.Services.AddSingleton<MongoDBService>();
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(
+        options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 //builder.Services.AddDbContext<DataContext>(options => {
 //    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 //});
-//builder.Services.AddCors(options => options.AddPolicy(name: "SuperHeroOrigins",
-//    policy =>
-//    {
-//        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
-//    }));
+builder.Services.AddCors(options => options.AddPolicy(name: "SuperHeroOrigins",
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+    }));
 
 var app = builder.Build();
 
